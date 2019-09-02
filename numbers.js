6c5cfae693a5acced7dc519850273b532d5ff7b2
Namugumya Sandra <salyn134@gmail.com>
2019-09-02 09:40:27 +0300

    Refactor numbers logic

--- numbers.js
+++ numbers.js
@@ -1,12 +1,9 @@
 
 // changing numbers into words
-let num = ['','one','two','three','four','five','six','seven','eight','nine','ten','eleven','twelve','thirteen','fourteen','fifteen'];
+let num = ['one','two','three','four','five','six','seven','eight','nine','ten','eleven','twelve','thirteen','fourteen','fifteen'];
 let myObject = {};
-// let myObject = new Map();
-
-for(let i=1; i < num.length; i++){
-    // myObject.set(num[i],Math.pow(i,2))
-    myObject[num[i]] = Math.pow(i,2);
+for(let i=0; i < num.length; i++){
+    myObject[num[i]] = Math.pow(i+1,2);
 }
 console.log(myObject); 
 return myObject; 
