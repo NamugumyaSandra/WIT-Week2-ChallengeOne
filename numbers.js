892d40a4142e7b08b454a90529f32baa4a6020e2
Namugumya Sandra <salyn134@gmail.com>
2019-09-12 13:59:46 +0300

    Refafactor the numbers logic.

--- numbers.js
+++ numbers.js
@@ -1,10 +1,9 @@
 
 // changing numbers into words
-let num = ['one','two','three','four','five','six','seven','eight','nine','ten','eleven','twelve','thirteen','fourteen','fifteen'];
 let myObject = {};
-for(let i=0; i < num.length; i++){
-    myObject[num[i]] = Math.pow(i+1,2);
+for(let i = 1; i < 16; i++){
+    myObject[i] = Math.pow(i,2);
 }
-console.log(myObject); 
+console.log(myObject);
 return myObject; 
 
